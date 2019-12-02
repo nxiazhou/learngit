23a2c646bd5de78c51ccf9bf0a7c4efd43219dd0
nxiazhou <1217205772@qq.com>
2019-12-02 20:32:23 +0800

    use callback to update

--- b.c
+++ b.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
+typedef void(*FUNPTR)(int);//指针类型，指向一个函数
 typedef struct node
 {
     int data;
     struct node *next; /*定义一个结构*/ //foo是类型不是变量
 } NODE,*NODEPTR;//未完成类型
 
-void traverse(NODEPTR head)
+void traverse(NODEPTR head,FUNPTR callback)//callback 为回调函数
 {
     NODEPTR p= head;
-    while(p!=NULL)
+    while(p!=NULL)//改成p也行
     {
-        printf("%d\n",p->data);
+        callback(p->data);
         p=p->next;
     }
+}
+
+void print(int v)
+{
+    printf("%d\n",v)
 }
 int main()
 {
